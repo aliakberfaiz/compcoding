e1abe85c1dacb40d20d6d0a2734a2939c2e419af
aliakberfaiz <aliakbar.faiz@gmail.com>
2016-10-18 16:23:25 +0000

    Revert "Adding new codes and Readme"

--- Hackerearth/Practice/Basic Programming/Palindromic String.py
+++
@@ -1,9 +0,0 @@
-#LINK - https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/palindrome-check-2/
-
-s = input()
-r = s[::-1]
-
-if ( s==r ) :
-	print("YES")
-else :
-	print("NO")
